899bdefa7160cc0d41df90badd69234061a6bd61
Zoay <jkanmogne@gmail.com>
2014-12-21 14:43:32 +0100

    add new features to the resume, by only using if and for statements

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,18 +9,33 @@ var bio = {
         "loc": "Paris"
     },
     "welcomeMsg": "Hello, I'm Joey. I'm 23 years old and I live in Paris",
-    "skills": ['Java', ' SQL', ' JUnit', ' Hadoop', ' HBase', ' MapReduce']
+    "skills": ['Java', ' SQL', ' JUnit', ' Hadoop', ' HBase', ' MapReduce'],
+    "bioPic": "./images/fry.jpg"
 };
 
 //
 // Work
 ////////////
-var work = {
-    "position": "Intern",
-    "employer": "Dassault Systèmes",
-    "year": 0.5,
-    "city": "Velizy-Villacoublay",
-    "description": "Creation d'un widget d'édition d'exigences avec contenu riche"
+var works = {
+	"jobs":[
+		{
+		    "title": "Intern",
+		    "employer": "Dassault Systèmes",
+		    "year": "6 months",
+		    "dates": "July 01st - December 19, 2014",
+		    "city": "Velizy-Villacoublay",
+		    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		},
+
+		{
+		    "title": "Intern",
+		    "employer": "Qarnot Computing",
+		    "year": "1 month",
+		    "dates": "July 01st - August 08, 2013",
+		    "city": "Montrouge",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		}
+	]
 };
 
 //
@@ -32,19 +47,19 @@ var projects = {
         {
             "title": "Interface Blender Python",
             "dates": "07/07 - 08/07",
-            "description": "Interface Blender Python"
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
         },
 
         {
             "title": "Index Inverse avec HDFS",
             "dates": "07/07 - 08/07",
-            "description": "Construction d'un index Inverse avec MapReduce"
+           "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
         },
 
         {
             "title": "Index Inverse avec HBase",
             "dates": "07/07 - 08/07",
-            "description": "Construction d'un index Inverse avec MapReduce"
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
         }
     ]
 };
@@ -98,9 +113,76 @@ var education = {
         }
     ]
 };
+var headerName = HTMLheaderName.replace('%data%',bio.name);
+var headerRole = HTMLheaderRole.replace('%data%',bio.role);
+var WelcomeMsg = HTMLWelcomeMsg.replace('%data%',bio.welcomeMsg);
+var mobile = HTMLmobile.replace('%data%',bio.contact.mobile);
+var email = HTMLemail.replace('%data%',bio.contact.email);
+var twitter = HTMLtwitter.replace('%data%',bio.contact.twitter);
+var github = HTMLgithub.replace('%data%',bio.contact.github);
+var hlocation = HTMLlocation.replace('%data%',bio.contact.loc);
+// var skills = HTMLskills.replace('%data%',bio.skills);
+var pic = HTMLbioPic.replace('%data%',bio.bioPic);
 
+var bioSkills = bio.skills;
 
+// console.log('Before if statement');
+$('#header').prepend(WelcomeMsg);
+$('#header').prepend(headerRole);
+$('#header').prepend(headerName);
 
+$('#header').append(pic);
+$('#topContacts').append(mobile);
+$('#topContacts').append(email);
+$('#topContacts').append(twitter);
+$('#topContacts').append(github);
+$('#topContacts').append(hlocation);
+
+if(bioSkills.length > 0){
+	$('#header').append(HTMLskillsStart);
+	
+	/*var skillFormat = HTMLskills.replace("%data%", bioSkills[0]);
+	$('#skills').append(skillFormat);
+
+	skillFormat = HTMLskills.replace("%data%", bioSkills[1]);
+	$('#skills').append(skillFormat);
+	
+	skillFormat = HTMLskills.replace("%data%", bioSkills[2]);
+	$('#skills').append(skillFormat);
+	skillFormat = HTMLskills.replace("%data%", bioSkills[3]);
+	$('#skills').append(skillFormat);
+	skillFormat = HTMLskills.replace("%data%", bioSkills[4]);
+	$('#skills').append(skillFormat);
+	skillFormat = HTMLskills.replace("%data%", bioSkills[5]);
+	$('#skills').append(skillFormat);*/
+	/*var skillFormat = HTMLskills.replace("%data%", bioSkills);
+	$('#skills').append(skillFormat);*/
+
+	var formattedSkill;
+	for(skill in bioSkills)
+	{
+		formattedSkill = HTMLskills.replace("%data%", bioSkills[skill]);
+		$('#skills').append(formattedSkill);	
+	}
+}
+
+// console.log('After if statement');
+for(job in works.jobs)
+{
+	$('#workExperience').append(HTMLworkStart);
+	console.log(works.jobs)
+	console.log(job);
+
+	var title = HTMLworkTitle.replace("%data%", works.jobs[job]['title']);
+	var employer = HTMLworkEmployer.replace("%data%", works.jobs[job]['employer']);
+	var dates = HTMLworkDates.replace("%data%",  works.jobs[job].dates);
+	var description = HTMLworkDescription.replace("%data%",  works.jobs[job].description);
+
+	var titEmployer = employer + title;
+	$('.work-entry:last').append(titEmployer);
+	$('.work-entry:last').append(dates);
+	$('.work-entry:last').append(description);
+}
 
 
 
